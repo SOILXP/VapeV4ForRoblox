cc913304a9ef3668f1d4cad5a4b20aceb78079b5
SOILXP <116461029+SOILXP@users.noreply.github.com>
2026-04-26 16:07:41 -0400

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -3903,7 +3903,7 @@ function mainapi:CreateCategory(categorysettings)
 		icon.Position = UDim2.fromOffset(12, 8)
 		icon.ImageTransparency = 0
 		icon.Image = getcustomasset('newvape/assets/new/favoriteoff.png')
-		icon.ImageColor3 = Color3.new(1, 1, 1)
+		icon.ImageColor3 = uipallet.Text
 	end
 	local title = Instance.new('TextLabel')
 	title.Name = 'Title'
